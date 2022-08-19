79ad1822d58140ccf60e58b52c51cb1bc483528e
HEGADE <subramanyashegade@gmail.com>
2022-08-19 23:36:18 +0530

    Added json format for models

--- src/models/place.go
+++ src/models/place.go
@@ -4,14 +4,20 @@ import (
 	"gorm.io/gorm"
 )
 
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 type City struct {
 	gorm.Model
-	Name    string
-	StateID int
-	State   State
+	Name    string `json:"name"`
+	StateID int    `json:"StateID"`
+	State   State  `json:"State"`
 }
 
 type State struct {
-	ID   int
-	Name string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
